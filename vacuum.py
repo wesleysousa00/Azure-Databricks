5cb4f1d469a416def01cdbf68cc91dc7d8797964
Wesley Sousa <93266288+wesleysousa00@users.noreply.github.com>
2023-09-14 23:32:49 -0300

    Add files via upload

---
+++ vacuum.py
@@ -0,0 +1,17 @@
+# Vacuum routine for cleaning files > 7 days
+
+from delta.tables import *
+
+databases = sqlContext.sql("SHOW DATABASES").collect()
+
+for db in databases:
+    db_name = db.databaseName if db.databaseName != "default" else None
+
+    if db_name: 
+        tables = sqlContext.sql(f"SHOW TABLES IN {db_name}").select(["database", "tableName"]).collect()
+        
+        for row in tables:
+            delta_table_name = f"{row.database}.{row.tableName}"
+
+            delta_table = DeltaTable.forName(spark, delta_table_name)
+            delta_table.vacuum(num_hours)
